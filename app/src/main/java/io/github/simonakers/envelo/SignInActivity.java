4c3a9723a2ba9699875832781f230e22547f4e44
Shayes <hayesakers@gmail.com>
2022-05-29 18:12:12 -0400

    Sign in working (not fully tested but no errors).

--- app/src/main/java/io/github/simonakers/envelo/SignInActivity.java
+++ app/src/main/java/io/github/simonakers/envelo/SignInActivity.java
@@ -1,77 +1,85 @@
 package io.github.simonakers.envelo;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.Scope;
 
-public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
-    GoogleSignInClient gsClient;
-    ActivityResultLauncher<Intent> signIn;
+public class SignInActivity extends AppCompatActivity {
+    /** A callback for when the user finishes signing in */
+    private final ActivityResultCallback<ActivityResult> onActivityResult = result -> {
+        if (result.getResultCode() == RESULT_OK) {
+            showMain();
+        } else {
+            showLoginFailed();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in_activity);
 
+        // Register a callback for when the user finishes sign in
+        ActivityResultLauncher<Intent> signIn = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            onActivityResult
+        );
+
+        // Create the sign in options
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
+            .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+            .build();
 
-        gsClient = GoogleSignIn.getClient(this, gso);
-
-        signIn = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), uri -> signIn());
-
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        // Set the callback for the sign in button
+        GoogleSignInClient gsc = GoogleSignIn.getClient(this, gso);
+        findViewById(R.id.sign_in_button).setOnClickListener(view -> signIn.launch(gsc.getSignInIntent()));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-
-        if (account != null) {
-            showMain();
-        }
+        // If already logged in, skip to main menu
+        if (isLoggedIn()) showMain();
     }
 
-    @Override
-    public void onClick(View view) {
-        signIn.launch(gsClient.getSignInIntent());
+    /**
+     * Checks if the user is already logged in.
+     *
+     * @return true if the user is already logged in, false if not
+     */
+    private boolean isLoggedIn() {
+        return GoogleSignIn.getLastSignedInAccount(this) != null;
     }
 
-    private void signIn() {
-        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(gsClient.getSignInIntent());
-
-        try {
-            GoogleSignInAccount account = task.getResult(ApiException.class);
-            // Signed in successfully, show authenticated UI.
-            showMain();
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.v("shay", "signInResult:failed code=" + e.getStatusCode());
-
-            Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG).show();
-        }
+    /**
+     * Displays a toast to the user saying that something went wrong during sign in.
+     */
+    private void showLoginFailed() {
+        Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG).show();
     }
 
+    /**
+     * Shows the main activity and closes this activity.
+     */
     private void showMain() {
+        // If not logged in, say that login failed but proceed to next activity anyways
+        if (!isLoggedIn()) showLoginFailed();
+
+        // Start main activity and clear activity backstack
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
