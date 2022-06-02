a776e9ff10373bb39fd863cc59d2bf840c468137
Shayes <hayesakers@gmail.com>
2022-06-01 20:22:19 -0400

    Added top toolbar. Some minor styling.

--- app/src/main/java/io/github/simonakers/envelo/MainActivity.java
+++ app/src/main/java/io/github/simonakers/envelo/MainActivity.java
@@ -3,6 +3,7 @@ package io.github.simonakers.envelo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set status bar color
+        TypedValue value = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, value, true);
+        getWindow().setStatusBarColor(value.data);
     }
 }
