09d6b94e5fd62456d60879ecb5e09747a591e515
Shayes <hayesakers@gmail.com>
2022-07-02 17:13:16 -0400

    Created date picker and utilities.

---
+++ app/src/main/java/io/github/simonakers/envelo/util/LocalDateUtils.java
@@ -0,0 +1,12 @@
+package io.github.simonakers.envelo.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateUtils {
+    public static DateTimeFormatter formatterFull = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
+
+    public static String formatFull(LocalDate date) {
+        return date.format(formatterFull);
+    }
+}
