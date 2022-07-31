4598fe1ef3aeef5fe6f8737ab6e0aa4e0aa3692b
Shayes <hayesakers@gmail.com>
2022-07-30 21:51:52 -0400

    Minor work on new categories.

--- app/src/main/java/net/shayes/envelo/view/NewCategoryActivity.java
+++ app/src/main/java/net/shayes/envelo/view/NewCategoryActivity.java
@@ -3,14 +3,36 @@ package net.shayes.envelo.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import net.shayes.envelo.R;
+import net.shayes.envelo.database.Category;
 
 public class NewCategoryActivity extends AppCompatActivity {
+    private EditText edtName;
+    private EditText edtAmount;
+
+    private int type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_category);
+
+        edtName = ((TextInputLayout) findViewById(R.id.til_name)).getEditText();
+        edtAmount = findViewById(R.id.edt_amount);
+
+        findViewById(R.id.fab).setOnClickListener(this::onClickFab);
+
+        type = getIntent().getIntExtra("categoryType", 0);
+    }
+
+    private void onClickFab(View view) {
+        String name = edtName.getText().toString();
+
     }
 }
